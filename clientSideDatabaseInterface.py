a47dfd5cc166ac523079b3068770fcf96efb4cd7
SciFI101 <havenhennellyh@gmail.com>
2020-01-15 10:00:09 -0700

    Third commit

--- clientSideDatabaseInterface.py
+++ clientSideDatabaseInterface.py
@@ -1,23 +1,19 @@
 
 #Python 3.6.5
-#This is just a simple interface for the database
-import csv
-import json
-from os import path
+import mysql.connector
+from mysql.connector import errorcode
 
-#csvfile = csv.DictReader(open("./database/diseaseSymptomDB.csv"))
-#for row in csvfile:
-#    print(row)
-
-#print("Json Time!")
-
-def lookup(query):
-    #with open("./database/diseaseSymptomDB.json") as path:
-    jsonfile = json.load(open("./database/diseaseSymptomDB.json"))
-    for i in range(len(jsonfile)):
-        if query.lower in jsonfile:
-            return jsonfile[query]
-        else:
-            return "Nothin\' boss, sorry"
-
-print(lookup("depression mental"))
+try:
+  cnx = mysql.connector.connect(user='root', 
+                                password='artemisfowl',
+                                host='127.0.0.1',
+                                database='actor')
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with your user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+else:
+  cnx.close()sssssssssssssssssssssssssssssssssssss
